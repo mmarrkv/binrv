0d08ed796c814297cc285cd0f3cd18c28e50d922
blockio <mmarrkv@yahoo.com>
2019-04-17 13:54:47 +0200

    showsyms.sh

--- nss-3.43/showsyms.sh
+++ nss-3.43/showsyms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 readelf -s ./dist/Debug/lib/libfreeblpriv3.so | grep DH_
+readelf -s ./dist/Debug/lib/libfreeblpriv3.so | grep RSA
+readelf -s ./dist/Debug/lib/libfreeblpriv3.so | grep AES
 
 
