34d150890da30794316c36578be6475e2f38d989
blockio <mmarrkv@yahoo.com>
2019-04-24 13:37:16 +0200

    nssmon in plain frida 8

--- nssmon/nssmon.py
+++ nssmon/nssmon.py
@@ -3,128 +3,92 @@ import frida
 import sys
 
 """
-Cmdline argument: pid of chrome's Utility: Network Service process. Check it out from Shift+Esc
-Requires: Replacing /usr/lib/x86_64-linux-gnu/nss/libfreeblpriv3.so with one re-compiled with debug symbols
+Requires python3
+Cmdline argument: pid of chrome's Utility: Network Service process. Check it out from Shift+Esc ?? + main browser process + --no-sandbox --no-zygote
+Cmdline argument: firefox main process
+Requires: Replacing /usr/lib/x86_64-linux-gnu/nss/libfreeblpriv3.so with one re-compiled with debug symbols ?? does it?
 """
 
 session = frida.attach(int(sys.argv[1]))
 script = session.create_script("""
-    send("nssmon start");
-    /*send(DebugSymbol.findFunctionsMatching("recv*").map(DebugSymbol.fromAddress).join());
-    send(DebugSymbol.findFunctionsMatching("send*").map(DebugSymbol.fromAddress).join());
-    send(DebugSymbol.findFunctionsMatching("DH_*").map(DebugSymbol.fromAddress).join());
-    send(DebugSymbol.findFunctionsMatching("*AES*").map(DebugSymbol.fromAddress).join());
-    send(DebugSymbol.findFunctionsMatching("*RSA*").map(DebugSymbol.fromAddress).join());*/
-    send("nssmon end");
-
-    /*var recvfs = DebugSymbol.findFunctionsMatching("recv*");
-    var arrayLength = recvfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(recvfs[i]).toString());
-        Interceptor.attach(recvfs[i], {
-          onEnter: function (args) {
-            send("in: recv*");
-          },
-          onLeave: function (retval) {
-            send("out: recv*");
-          }
-        });
+    send("nssmon init start");
+    send("Debug: "+Process.id);
+    send("Debug: "+Process.getCurrentThreadId());
+    var threads = Process.enumerateThreadsSync();
+    for (var i = 0; i < threads.length; i++) {
+        send("Debug: "+threads[i].id);
     }
-
-    var sendfs = DebugSymbol.findFunctionsMatching("send*");
-    var arrayLength = sendfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(sendfs[i]).toString());
-        Interceptor.attach(sendfs[i], {
-          onEnter: function (args) {
-            send("in: send*");
-          },
-          onLeave: function (retval) {
-            send("out: send*");
-          }
-        });
-    }*/
-
-    var aesfs = DebugSymbol.findFunctionsMatching("*AES*");
-    var arrayLength = aesfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(aesfs[i]).toString());
-        Interceptor.attach(aesfs[i], {
-          onEnter: function (args) {
-            send("in: *AES*");
-          },
-          onLeave: function (retval) {
-            send("out: *AES*");
-          }
-        });
+    /*
+    var modules = Process.enumerateModules();
+    for (var i = 0; i < modules.length; i++) {
+        send("Debug: "+modules[i].name);
     }
+    */
 
-    var rsafs = DebugSymbol.findFunctionsMatching("*RSA*");
-    var arrayLength = rsafs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(rsafs[i]).toString());
-        Interceptor.attach(rsafs[i], {
-          onEnter: function (args) {
-            send("in: *RSA* ");
-            var nparray = Thread.backtrace(this.context, Backtracer.ACCURATE);
-            for (var j = 0; j < nparray.length; j++) {
-                send(DebugSymbol.fromAddress(nparray[j]));
-            }
-          },
-          onLeave: function (retval) {
-            send("out: *RSA*");
-          }
-        });
-    }
+    var np_prconnect = DebugSymbol.getFunctionByName('PR_Connect');
+    send("Debug: PR_Connect@"+np_prconnect);
+    var res = Interceptor.attach(np_prconnect, {
+      onEnter: function (args) {
+        send("in: PR_Connect "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PR_Connect "+this.threadId);
+      }
+    });
+    send("Debug: PR_Connect attach res"+res.toString());
 
-    var dhfs = DebugSymbol.findFunctionsMatching("*DH_*");
-    var arrayLength = dhfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(dhfs[i]).toString());
-        Interceptor.attach(dhfs[i], {
-          onEnter: function (args) {
-            send("in: *DH_*");
-          },
-          onLeave: function (retval) {
-            send("out: *DH_*");
-          }
-        });
-    }
+    var np_prnewtcpsocket = DebugSymbol.getFunctionByName('PR_NewTCPSocket');
+    send("Debug: PR_NewTCPSocket@"+np_prnewtcpsocket);
+    Interceptor.attach(np_prnewtcpsocket, {
+      onEnter: function (args) {
+        send("in: PR_NewTCPSocket "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PR_NewTCPSocket "+this.threadId);
+      }
+    });
 
-    var rdfs = DebugSymbol.findFunctionsMatching("*rijndael*");
-    var arrayLength = rdfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(rdfs[i]).toString());
-        Interceptor.attach(rdfs[i], {
-          onEnter: function (args) {
-            send("in: *rijndael*");
-          },
-          onLeave: function (retval) {
-            send("out: *rijndael*");
-          }
-        });
-    }
+    var np_prsend = DebugSymbol.getFunctionByName('PR_Send');
+    send("Debug: PR_Send@"+np_prsend);
+    Interceptor.attach(np_prsend, {
+      onEnter: function (args) {
+        send("in: PR_Send "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PR_Send "+this.threadId);
+      }
+    });
 
-    var pkfs = DebugSymbol.findFunctionsMatching("PK11_*");
-    var arrayLength = pkfs.length;
-    for (var i = 0; i < arrayLength; i++) {
-        send("Debug: "+DebugSymbol.fromAddress(pkfs[i]).toString());
-        Interceptor.attach(pkfs[i], {
-          onEnter: function (args) {
-            send("in: PK_*");
-          },
-          onLeave: function (retval) {
-            send("out: PK_*");
-          }
-        });
+    var np_pk11cipherop = DebugSymbol.getFunctionByName('PK11_CipherOp');
+    send("Debug: PK11_CipherOp@"+np_pk11cipherop);
+    Interceptor.attach(np_pk11cipherop, {
+      onEnter: function (args) {
+        send("in: PK11_CipherOp "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PK11_CipherOp "+this.threadId);
+      }
+    });
 
-    }
+    var np_pk11derive = DebugSymbol.getFunctionByName('PK11_Derive');
+    send("Debug: PK11_Derive@"+np_pk11derive);
+    Interceptor.attach(np_pk11derive, {
+      onEnter: function (args) {
+        send("in: PK11_Derive "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PK11_Derive"+this.threadId);
+      }
+    });
 
+    
+    Interceptor.flush();
+    send("nssmon init end");
 
 """  )
 
-def on_message(message, data):
-    print(message)
+def on_message(message, data): 
+    print(message) # outstream to Yara
 script.on('message', on_message)
 script.load()
 sys.stdin.read()
