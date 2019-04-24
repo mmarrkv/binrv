dbbf31901d34bb53ca820684308348aa85cfb8c4
blockio <mmarrkv@yahoo.com>
2019-04-24 16:02:50 +0200

    nssmon in plain frida 9

--- nssmon/nssmon.py
+++ nssmon/nssmon.py
@@ -4,9 +4,8 @@ import sys
 
 """
 Requires python3
-Cmdline argument: pid of chrome's Utility: Network Service process. Check it out from Shift+Esc ?? + main browser process + --no-sandbox --no-zygote
 Cmdline argument: firefox main process
-Requires: Replacing /usr/lib/x86_64-linux-gnu/nss/libfreeblpriv3.so with one re-compiled with debug symbols ?? does it?
+In case you need to attach to web content process: $MOZ_DISABLE_CONTENT_SANDBOX=1
 """
 
 session = frida.attach(int(sys.argv[1]))
@@ -14,28 +13,73 @@ script = session.create_script("""
     send("nssmon init start");
     send("Debug: "+Process.id);
     send("Debug: "+Process.getCurrentThreadId());
+
     var threads = Process.enumerateThreadsSync();
     for (var i = 0; i < threads.length; i++) {
         send("Debug: "+threads[i].id);
     }
-    /*
+
     var modules = Process.enumerateModules();
     for (var i = 0; i < modules.length; i++) {
         send("Debug: "+modules[i].name);
     }
-    */
+
 
     var np_prconnect = DebugSymbol.getFunctionByName('PR_Connect');
     send("Debug: PR_Connect@"+np_prconnect);
     var res = Interceptor.attach(np_prconnect, {
       onEnter: function (args) {
         send("in: PR_Connect "+this.threadId);
+        send("fd="+args[0]);
       },
       onLeave: function (retval) {
         send("out: PR_Connect "+this.threadId);
       }
     });
     send("Debug: PR_Connect attach res"+res.toString());
+
+
+    var np_prshutdown = DebugSymbol.getFunctionByName('PR_Shutdown');
+    send("Debug: PR_Shutdown@"+np_prshutdown);
+    var res = Interceptor.attach(np_prshutdown, {
+      onEnter: function (args) {
+        send("in: PR_Shutdown "+this.threadId);
+        send("fd="+args[0]);
+      },
+      onLeave: function (retval) {
+        send("out: PR_Shutdown "+this.threadId);
+      }
+    });
+    send("Debug: PR_Shutdown attach res"+res.toString());
+
+
+    var np_prclose = DebugSymbol.getFunctionByName('PR_Close');
+    send("Debug: PR_Close@"+np_prclose);
+    var res = Interceptor.attach(np_prclose, {
+      onEnter: function (args) {
+        send("in: PR_Close "+this.threadId);
+        send("fd="+args[0]);
+      },
+      onLeave: function (retval) {
+        send("out: PR_Close "+this.threadId);
+      }
+    });
+    send("Debug: PR_Close attach res"+res.toString());
+
+
+    var np_sslimportfd = DebugSymbol.getFunctionByName('SSL_ImportFD');
+    send("Debug: SSL_ImportFD@"+np_sslimportfd);
+    Interceptor.attach(np_sslimportfd, {
+      onEnter: function (args) {
+        send("in: SSL_ImportFD "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: SSL_ImportFD "+this.threadId);
+        send("ret="+retval);
+      }
+    });
+
+
 
     var np_prnewtcpsocket = DebugSymbol.getFunctionByName('PR_NewTCPSocket');
     send("Debug: PR_NewTCPSocket@"+np_prnewtcpsocket);
@@ -59,14 +103,28 @@ script = session.create_script("""
       }
     });
 
-    var np_pk11cipherop = DebugSymbol.getFunctionByName('PK11_CipherOp');
-    send("Debug: PK11_CipherOp@"+np_pk11cipherop);
-    Interceptor.attach(np_pk11cipherop, {
+
+    var np_authcert = DebugSymbol.getFunctionByName('SSL_AuthCertificateHook');
+    send("Debug: SL_AuthCertificateHook@"+np_authcert);
+    Interceptor.attach(np_authcert, {
       onEnter: function (args) {
-        send("in: PK11_CipherOp "+this.threadId);
+        send("in: SSL_AuthCertificateHook "+this.threadId);
       },
       onLeave: function (retval) {
-        send("out: PK11_CipherOp "+this.threadId);
+        send("out: SSL_AuthCertificateHook "+this.threadId);
+        send("ret="+retval);
+      }
+    });
+
+
+    var np_pk11enc = DebugSymbol.getFunctionByName('PK11_Encrypt');
+    send("Debug: PK11_Encrypt@"+np_pk11enc);
+    Interceptor.attach(np_pk11enc, {
+      onEnter: function (args) {
+        send("in: PK11_Encrypt "+this.threadId);
+      },
+      onLeave: function (retval) {
+        send("out: PK11_Encrypt "+this.threadId);
       }
     });
 
@@ -77,7 +135,7 @@ script = session.create_script("""
         send("in: PK11_Derive "+this.threadId);
       },
       onLeave: function (retval) {
-        send("out: PK11_Derive"+this.threadId);
+        send("out: PK11_Derive "+this.threadId);
       }
     });
 
