8539739b53eabcdfdb5374302b02277300f7490a
blockio <mmarrkv@yahoo.com>
2019-04-29 14:26:04 +0200

    yara 4

--- yara_rules/nssmon.py
+++ yara_rules/nssmon.py
@@ -9,14 +9,14 @@ def mycallback(data):
     dictSslImportFd = data['strings']
     for line in dictSslImportFd:      
         #print(str(line[2]))
-        matchObj = re.match( r'.+?ret:(.+?)\\n', str(line[2]), re.I)
+        matchObj = re.match( r'.+?fd:(.+?)\\n', str(line[2]), re.I)
         if matchObj:
             #print('match:'+matchObj.group(1))
             fds.append(matchObj.group(1))
 
     return yara.CALLBACK_CONTINUE
 
-fh = open('./binrvff.src')
+fh = open('./sslauthcertcompl.src')
 rules = yara.compile(file=fh)
 fh.close()
 
