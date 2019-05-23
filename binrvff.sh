84af65ed83b0d3f8f9ee06b628e97d68b073cd78
blockio <mmarrkv@yahoo.com>
2019-05-23 13:30:22 +0200

    EC validation traces

--- binrvff.sh
+++ binrvff.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo frida-trace -p $1 -i "SSL_ImportFD" -i "PR_Connect" -i "SSL_AuthCertificateHook" -i "SSL_AuthCertificateComplete" -i "SECKEY_CreateDHPrivateKey" -i "SECKEY_CreateECPrivateKey" -i "SECKEY_DestroyPrivateKey" -i "*VerifySignedData*" -i "*Derive*" -i "PK11_Encrypt"  -i "PR_Send" -i "PR_Close"
+#requires: pid of main firefox process, debug symbols for libfreeblpriv3.so (for EC*)
+
+sudo frida-trace -p $1 -i "SSL_ImportFD" -i "PR_Connect" -i "SSL_AuthCertificateHook" -i "SSL_AuthCertificateComplete" -i "SECKEY_CreateDHPrivateKey" -i "SECKEY_CreateECPrivateKey" -i "SECKEY_DestroyPrivateKey" -i "*VerifySignedData*" -i "*Derive*" -i "PK11_Encrypt"  -i "PR_Send" -i "PR_Close" -s "EC_ValidatePublicKey" -s "ECPoint_validate" 
