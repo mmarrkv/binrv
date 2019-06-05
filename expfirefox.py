d3ee859185c9179d1bf09f717844bb3cda580eb4
blockio <mmarrkv@yahoo.com>
2019-06-05 16:53:20 +0200

    exps 3

--- expfirefox.py
+++ expfirefox.py
@@ -1,6 +1,7 @@
 # requires sudo -H pip3 install -U selenium
 # requires curl
-# has to be reside in binrv root along with binrvff.sh
+# has to reside in binrv root along with binrvff.sh
+# requires alexatop.csv
 # yara_rules and exp1 as subdirectories
 # creates traces in exp1/traces
 # creates results in exp1/results
@@ -18,12 +19,13 @@ import threading
 import time
 import glob
 import socket
+import csv
 
 def start_dbi(tracefilename):
     print('Setting up DBI...')
     fo = os.popen("pidof firefox | awk '{print $NF}'")
     pid = fo.readline().strip('\r\n')
-    os.system('binrvff.sh '+pid+' > exp1/traces/'+tracefilename)    
+    os.system('binrvff.sh '+pid+' > exp1/traces/trace_'+tracefilename)    
 
 
 if __name__ == "__main__":
@@ -32,21 +34,27 @@ if __name__ == "__main__":
     options = Options()
     options.add_argument('-headless')
 
+    print('Reading in urls...')
+    with open('alexatop.csv', 'r') as f:
+        reader = csv.reader(f)
+        urls = list(reader)
+        #print(urls[0][1])
+
     print('Start tracing sessions...')
-    for i in range(1,4):
+    for i in range(0,3):
         driver = Firefox(executable_path='geckodriver', options=options)
         wait = WebDriverWait(driver, timeout=10)
 
-        mytracefilename='google.com'+str(i)
+        mytracefilename=urls[i][1]
         threading.Thread(target=start_dbi, args=[mytracefilename]).start()
         time.sleep(5)
 
         print('Continuing...')
-        driver.get('http://www.google.com')
+        driver.get('https://'+mytracefilename)
         driver.quit()
 
     print('Expanding to subtraces...')
-    for f in glob.glob('exp1/traces/*com*'):
+    for f in glob.glob('exp1/traces/trace_*'):
         os.system('python3 yara_rules/xtract.py '+'~/binrv/'+f+' > /dev/null')    
 
     print('Executing yara rules on all files...')
@@ -57,5 +65,6 @@ if __name__ == "__main__":
     print('Obtaining ground truth...')
     #ip=socket.gethostbyname('google.com')
     #os.system('curl -o exp1/truth/google.com https://api.ssllabs.com/api/v3/getEndpointData?host=google.com&s='+ip)    
-    os.system('curl -o exp1/truth/google.com https://api.ssllabs.com/api/v3/analyze?host=google.com')    
+    for i in range(0,3):
+        os.system('curl -o exp1/truth/'+urls[i][1] +' https://api.ssllabs.com/api/v3/analyze?host='+urls[i][1])    
 
