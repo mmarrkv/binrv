4de1d46134b0ec50ff4c8011778808285230213b
blockio <mmarrkv@yahoo.com>
2019-06-06 11:04:55 +0200

    exps 5

--- expfirefox2.py
+++ expfirefox2.py
@@ -2,11 +2,11 @@
 # requires sudo -H pip3 install -U selenium
 # requires curl
 # has to reside in binrv root along with binrvff.sh
-# requires alexatop.csv
-# yara_rules and exp1 as subdirectories
-# creates traces in exp1/traces
-# creates results in exp1/results
-# creates ground truth in exp1/truth
+# requires badsll.csv
+# yara_rules and exp2 as subdirectories
+# creates traces in exp2/traces
+# creates results in exp2/results
+# creates ground truth in exp2/truth
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
@@ -26,23 +26,23 @@ def start_dbi(tracefilename):
     print('Setting up DBI...')
     fo = os.popen("pidof firefox | awk '{print $NF}'")
     pid = fo.readline().strip('\r\n')
-    os.system('binrvff.sh '+pid+' > exp1/traces/trace_'+tracefilename)    
+    os.system('binrvff.sh '+pid+' > exp2/traces/trace_'+tracefilename)    
 
 
 if __name__ == "__main__":
-    print('Experiment 1: Demonstrating binrv over 100 firefox sessions')
+    print('Experiment 2: Demonstrating binrv over 12 firefox sessions contacting known insecurely configured sites')
     print('Initializing...')
     options = Options()
     options.add_argument('-headless')
 
     print('Reading in urls...')
-    with open('alexatop.csv', 'r') as f:
+    with open('badssl.csv', 'r') as f:
         reader = csv.reader(f)
         urls = list(reader)
         #print(urls[0][1])
 
     print('Start tracing sessions...')
-    for i in range(0,100):
+    for i in range(0,12):
         driver = Firefox(executable_path='geckodriver', options=options)
         wait = WebDriverWait(driver, timeout=10)
 
@@ -59,17 +59,17 @@ if __name__ == "__main__":
         driver.quit()
 
     print('Expanding to subtraces...')
-    for f in glob.glob('exp1/traces/trace_*'):
+    for f in glob.glob('exp2/traces/trace_*'):
         os.system('python3 yara_rules/xtract.py '+'~/binrv/'+f+' > /dev/null')    
 
     print('Executing yara rules on all files...')
-    for f in glob.glob('exp1/traces/*_0x*'):
-        os.system('yara -s yara_rules/binrvff.src '+f+' > exp1/monitor/'+f.split("/")[2])    
+    for f in glob.glob('exp2/traces/*_0x*'):
+        os.system('yara -s yara_rules/binrvff.src '+f+' > exp2/monitor/'+f.split("/")[2])    
 
 
     print('Obtaining ground truth...')
     #ip=socket.gethostbyname('google.com')
-    #os.system('curl -o exp1/truth/google.com https://api.ssllabs.com/api/v3/getEndpointData?host=google.com&s='+ip)    
-    for i in range(0,100):
-        os.system('curl -o exp1/truth/'+urls[i][1] +' https://api.ssllabs.com/api/v3/analyze?host='+urls[i][1])    
+    #os.system('curl -o exp2/truth/google.com https://api.ssllabs.com/api/v3/getEndpointData?host=google.com&s='+ip)    
+    for i in range(0,12):
+        os.system('curl -o exp2/truth/'+urls[i][1] +' https://api.ssllabs.com/api/v3/analyze?host='+urls[i][1])    
 
