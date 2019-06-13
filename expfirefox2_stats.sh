e8ca915897dbc41c4351f961c3691b780ba501da
blockio <mmarrkv@yahoo.com>
2019-06-13 15:44:21 +0200

    exp run 25

--- expfirefox2_stats.sh
+++ expfirefox2_stats.sh
@@ -5,51 +5,51 @@ echo -e "Stats for exp2\n"
 echo -e "Rule matches for all extracted subtraces"
 echo -e "----------------------------------------"
 echo -n "certerr_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep -c certerr_accept
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c certerr_accept
 echo -n "certerr_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep -c certerr_bad
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c certerr_bad
 echo -n "masterkeyderive_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep -c masterkeyderive_accept
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c masterkeyderive_accept
 echo -n "masterkeyderive_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep -c masterkeyderive_bad
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c masterkeyderive_bad
 echo -n "scrubprivkey_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep -c scrubprivkey_accept
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c scrubprivkey_accept
 echo -n "scrubprivkey_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep -c scrubprivkey_bad
+cat exp2/monitor/*_0x* | grep -v ^0x | grep -c scrubprivkey_bad
 
 echo -e "\nGrouping per unique URL"
 echo -e "-------------------------"
 echo -e "\ncerterr_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 echo -e "\n\ncerterr_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 echo -e "\n\nmasterkeyderive_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 echo -e "\n\nmasterkeyderive_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 echo -e "\n\nscrubprivkey_accept:"
-cat exp2/monitor/* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 echo -e "\n\nscrubprivkey_bad:"
-cat exp2/monitor/* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp2/monitor/* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp2/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/12"
 
-echo -e "\nAlert per subtrace (sport the all-0 URL)"
+echo -e "\nAlert per subtrace (spot the all-0 URL)"
 echo -e "--------------------"
 ls -l exp2/monitor | awk '{print $5 "\t" $9}' | grep _0x
--- expfirefox_stats.sh
+++ expfirefox_stats.sh
@@ -5,51 +5,51 @@ echo -e "Stats for exp1\n"
 echo -e "Rule matches for all extracted subtraces"
 echo -e "----------------------------------------"
 echo -n "certerr_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep -c certerr_accept
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c certerr_accept
 echo -n "certerr_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep -c certerr_bad
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c certerr_bad
 echo -n "masterkeyderive_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep -c masterkeyderive_accept
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c masterkeyderive_accept
 echo -n "masterkeyderive_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep -c masterkeyderive_bad
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c masterkeyderive_bad
 echo -n "scrubprivkey_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep -c scrubprivkey_accept
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c scrubprivkey_accept
 echo -n "scrubprivkey_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep -c scrubprivkey_bad
+cat exp1/monitor/*_0x* | grep -v ^0x | grep -c scrubprivkey_bad
 
 echo -e "\nGrouping per unique URL"
 echo -e "-------------------------"
 echo -e "\ncerterr_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^certerr_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 echo -e "\n\ncerterr_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^certerr_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 echo -e "\n\nmasterkeyderive_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 echo -e "\n\nmasterkeyderive_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^masterkeyderive_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 echo -e "\n\nscrubprivkey_accept:"
-cat exp1/monitor/* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_accept | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 echo -e "\n\nscrubprivkey_bad:"
-cat exp1/monitor/* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c
 echo -n "Total:"
-cat exp1/monitor/* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
+cat exp1/monitor/*_0x* | grep -v ^0x | grep ^scrubprivkey_bad | awk -F " |_" {'print $4'} | uniq -c | grep -c .
 echo -e "/100"
 
-echo -e "\nAlert per subtrace (sport the all-0 URL)"
+echo -e "\nAlert per subtrace (spot the all-0 URL)"
 echo -e "--------------------"
 ls -l exp1/monitor | awk '{print $5 "\t" $9}' | grep _0x
